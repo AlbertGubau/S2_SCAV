e6ded26502a00c90e08f8bab62e8b4c6ee169caf
AlbertGubau <albert.gubau01@estudiant.upf.edu>
2022-10-21 11:33:52 +0200

    Task2

--- cut_N_seconds.py
+++ cut_N_seconds.py
@@ -6,7 +6,7 @@ def cut_n_seconds(in_file, out_file, n):
     if os.path.exists(out_file):
         os.remove(out_file)
 
-    command_line = 'ffmpeg -ss 00:00:00 -i ' + str(in_file) + '.mp4 -t 00:00:' + str(n) + ' -c copy ' + str(
+    command_line = 'ffmpeg -ss 00:00:03 -i ' + str(in_file) + '.mp4 -t 00:00:' + str(n) + ' -c copy ' + str(
         out_file) + '.mp4'
     print(command_line)
     os.system(command_line)
---
+++ yuv_histogram.py
@@ -0,0 +1,26 @@
+# This file is able to cut N seconds of the BBB video, Albert Gubau NIA: 229416
+import os
+
+
+def display_yuv_histogram(in_file, out_file):
+    if os.path.exists(out_file):
+        os.remove(out_file)
+
+    command_line = 'ffmpeg -i ' + str(in_file) + '.mp4 -vf "split=2[a][b], [b]histogram, format=yuva444p[hh],' \
+                                                 '[a][hh]overlay=x=W-w:y=H-h" -c:a copy ' + str(out_file) + '.mp4 '
+
+    # -c:a is due to re-encoding the video because we are filtering it
+
+
+    os.system(command_line)
+
+
+input_name = "BBB"
+boolean = False
+
+while not boolean:
+    output_name = str(input("Choose the name of the output file: "))
+    if output_name != input_name:
+        boolean = True
+
+display_yuv_histogram(input_name, output_name)
